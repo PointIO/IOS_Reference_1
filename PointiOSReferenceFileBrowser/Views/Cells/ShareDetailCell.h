56199db1a1963dbe2ee26885e2c2b6010e1064ec
jimmyboyle <jimmyboyle@jbimac27.home>
2013-06-15 18:47:53 -0400

    Update shareDetailViewController to display different icons for files and folders

---
+++ PointiOSReferenceFileBrowser/Views/Cells/ShareDetailCell.h
@@ -0,0 +1,16 @@
+//
+//  ShareDetailCell.h
+//  PointiOSReferenceFileBrowser
+//
+//  Created by jimmyboyle on 6/15/13.
+//  Copyright (c) 2013 PointIO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShareDetailCell : UITableViewCell
+
+@property (nonatomic, strong) IBOutlet UILabel      * nameLabel;
+@property (nonatomic, strong) IBOutlet UIImageView  * typeImage;
+
+@end
---
+++ PointiOSReferenceFileBrowser/Views/Cells/ShareDetailCell.m
@@ -0,0 +1,29 @@
+//
+//  ShareDetailCell.m
+//  PointiOSReferenceFileBrowser
+//
+//  Created by jimmyboyle on 6/15/13.
+//  Copyright (c) 2013 PointIO. All rights reserved.
+//
+
+#import "ShareDetailCell.h"
+
+@implementation ShareDetailCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
