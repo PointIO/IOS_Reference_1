96b6b00dfb117592695acf290707af0237c50752
jimboyle <jimboyle@jbmba.home>
2013-08-01 20:00:42 -0400

    Stub implementation of new Account View Controller

---
+++ PointiOSReferenceFileBrowser/Controllers/Generic/AccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  AccountViewController.h
+//  PointiOSReferenceFileBrowser
+//
+//  Created by jimboyle on 8/1/13.
+//  Copyright (c) 2013 PointIO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountViewController : UITableViewController
+
+@end
---
+++ PointiOSReferenceFileBrowser/Controllers/Generic/AccountViewController.m
@@ -0,0 +1,40 @@
+//
+//  AccountViewController.m
+//  PointiOSReferenceFileBrowser
+//
+//  Created by jimboyle on 8/1/13.
+//  Copyright (c) 2013 PointIO. All rights reserved.
+//
+
+#import "AccountViewController.h"
+
+@interface AccountViewController ()
+
+@end
+
+@implementation AccountViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
