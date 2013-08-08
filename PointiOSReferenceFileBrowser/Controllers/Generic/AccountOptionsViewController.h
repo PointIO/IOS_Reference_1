ffcdfd0909710fa34bda66d263c8613396de4ad2
jimboyle <jimboyle@jbmba.local>
2013-08-08 14:23:01 -0400

    Added "More" button on Login view to launch webView to Point.io Account Options WebPage

---
+++ PointiOSReferenceFileBrowser/Controllers/Generic/AccountOptionsViewController.h
@@ -0,0 +1,16 @@
+//
+//  AccountOptionsViewController.h
+//  PointiOSReferenceFileBrowser
+//
+//  Created by jimboyle on 8/8/13.
+//  Copyright (c) 2013 PointIO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountOptionsViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UIWebView *webView;
+
+
+@end
---
+++ PointiOSReferenceFileBrowser/Controllers/Generic/AccountOptionsViewController.m
@@ -0,0 +1,42 @@
+//
+//  AccountOptionsViewController.m
+//  PointiOSReferenceFileBrowser
+//
+//  Created by jimboyle on 8/8/13.
+//  Copyright (c) 2013 PointIO. All rights reserved.
+//
+
+#import "AccountOptionsViewController.h"
+
+@interface AccountOptionsViewController ()
+
+@end
+
+@implementation AccountOptionsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    _webView.autoresizingMask = (UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight);
+    NSURL *urlAddress = [NSURL URLWithString:@"http://www.point.io/account"];
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:urlAddress];
+    [_webView loadRequest:requestObj];
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
